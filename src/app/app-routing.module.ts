7b2782443123cc987ac55236662490dc8bf6afcc
Josue Krisanto <krisanto@yahoo.com>
2019-03-11 17:24:52 -0600

    Modificaciones en los modulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,15 @@
-import { NgModule } from '@angular/core';
+
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
 
+
 const routes: Routes = [
-  { 
-    path: 'inicio', 
-    component: PagesComponent,
-    children: [
-      { path: 'escritorio', component: DashboardComponent },
-      { path: 'progreso', component: ProgressComponent },
-     { path: 'graficas1', component: Graficas1Component },
-     { path: '', redirectTo: '/inicio', pathMatch: 'full' }
-    ]
-   },
-   { path: 'login', component: LoginComponent },
+
+     { path: 'login', component: LoginComponent },
    { path: 'registro', component: RegisterComponent },
-   // { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', component: NopagefoundComponent }
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const APP_ROUTES = RouterModule.forRoot(routes, { useHash: true});
